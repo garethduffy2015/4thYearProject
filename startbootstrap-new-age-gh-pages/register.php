56846d10fd4e331d87476928b85d0e8d418a52d7
x14428778 <garethduffy123@hotmail.com>
2017-12-04 12:26:57 +0000

    first commit

---
+++ startbootstrap-new-age-gh-pages/register.php
@@ -0,0 +1,269 @@
+<?php
+	ob_start();
+	session_start();
+	
+	include_once 'dbconnect.php';
+
+	$error = false;
+
+	if ( isset($_POST['btn-signup']) ) {
+		
+		// clean user inputs to prevent sql injections
+		$name = trim($_POST['name']);
+		$name = strip_tags($name);
+		$name = htmlspecialchars($name);
+		
+		$email = trim($_POST['email']);
+		$email = strip_tags($email);
+		$email = htmlspecialchars($email);
+		
+		$veremail = trim($_POST['veremail']);
+		$veremail = strip_tags($veremail);
+		$veremail = htmlspecialchars($veremail);
+		
+		
+		
+		$pass = trim($_POST['pass']);
+		$pass = strip_tags($pass);
+		$pass = htmlspecialchars($pass);
+		
+		// basic name validation
+		if (empty($name)) {
+			$error = true;
+			$nameError = "Please enter your full name.";
+		} else if (strlen($name) < 3) {
+			$error = true;
+			$nameError = "Name must have atleat 3 characters.";
+		} else if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+			$error = true;
+			$nameError = "Name must contain alphabets and space.";
+		}
+		
+		//basic email validation
+		if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+			$error = true;
+			$emailError = "Please enter valid email address.";
+		} else {
+			// check email exist or not
+			$query = "SELECT userEmail FROM users WHERE userEmail='$email'";
+			$result = mysql_query($query);
+			$count = mysql_num_rows($result);
+			if($count!=0){
+				$error = true;
+				$emailError = "Provided Email is already in use.";
+			}
+		}
+		
+		//basic email validation
+		if ( !filter_var($veremail,FILTER_VALIDATE_EMAIL) ) {
+			$error = true;
+			$veremailError = "Please enter valid email address.";
+		} else {
+			// check email exist or not
+			$query = "SELECT userEmail FROM users WHERE userEmail='$veremail'";
+			$result = mysql_query($query);
+			$count = mysql_num_rows($result);
+		}
+		// password validation
+		if (empty($pass)){
+			$error = true;
+			$passError = "Please enter password.";
+		} else if(strlen($pass) < 6) {
+			$error = true;
+			$passError = "Password must have atleast 6 characters.";
+		}
+		
+		// password encrypt using SHA256();
+		$password = hash('sha256', $pass);
+		
+		// if there's no error, continue to signup
+		if( !$error ) {
+			
+			$query = "INSERT INTO users(userName, userEmail, userverEmail, userPass) VALUES('$name','$email','$veremail','$password')";
+			$res = mysql_query($query);
+				
+			if ($res) {
+				$errTyp = "success";
+				$errMSG = "Successfully registered, you may login now";
+				unset($name);
+				unset($email);
+				unset($veremail);
+				unset($pass);
+			} else {
+				$errTyp = "danger";
+				$errMSG = "Something went wrong, try again later...";	
+			}	
+				
+		}
+		
+		
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>WPF</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="device-mockups/device-mockups.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/new-age.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Work Placement Feedback</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="index.php">Mainpage</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="index.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead">
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-lg-7 my-auto">
+            <div class="header-content mx-auto">
+              <h1 class="mb-5">National College of Ireland Work Placement Feedback System.</h1>
+            </div>
+          </div>
+          <div class="col-lg-5 my-auto">
+            <div class="device-container">
+              <div id="login-form">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+    
+    	<div class="col-md-12">
+        
+        	<div class="form-group">
+            	<h2 class="">Sign Up.</h2>
+            </div>
+        
+        	<div class="form-group">
+            	<hr />
+            </div>
+            
+            <?php
+			if ( isset($errMSG) ) {
+				
+				?>
+				<div class="form-group">
+            	<div class="alert alert-<?php echo ($errTyp=="success") ? "success" : $errTyp; ?>">
+				<span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+            	</div>
+                <?php
+			}
+			?>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+            	<input type="text" name="name" class="form-control" placeholder="Enter Name" maxlength="50" value="<?php echo $name ?>" />
+                </div>
+                <span class="text-danger"><?php echo $nameError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+            	<input type="email" name="email" class="form-control" placeholder="Enter Your Email" maxlength="40" value="<?php echo $email ?>" />
+                </div>
+                <span class="text-danger"><?php echo $emailError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+            	<input type="veremail" name="veremail" class="form-control" placeholder="Verify your Email" maxlength="255" value="<?php echo $veremail ?>" />
+                </div>
+                <span class="text-danger"><?php echo $veremailError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+            	<input type="password" name="pass" class="form-control" placeholder="Enter Password" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $passError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<hr />
+            </div>
+            
+            <div class="form-group">
+            	<button type="submit" class="btn btn-block btn-primary" name="btn-signup">Sign Up</button>
+            </div>
+            
+            <div class="form-group">
+            	<hr />
+            </div>
+            
+            <div class="form-group">
+            	<a href="index.php">Sign in Here...</a>
+            </div>
+        
+        </div>
+        
+   
+    </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+     <footer>
+      <div class="container">
+        <p>&copy; 2017 Gareth Duffy. All Rights Reserved.</p>
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="#">Privacy</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#">Terms</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#">FAQ</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+</body>
+</html>
+<?php ob_end_flush(); ?>
