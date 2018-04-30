c44cb029b8b230b85272920c80e34083af47cc5a
x14428778 <garethduffy123@hotmail.com>
2018-04-30 22:43:24 +0000

    second

---
+++ Feedback-System/home.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<html>
+    <head>
+        <title>Awesome Bubble Navigation with jQuery</title>
+
+        <link rel="stylesheet" href="css/mainpagestyle.css" type="text/css" media="screen"/>
+    
+    </head>
+    
+    
+
+<body>
+    
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="dash-box dash-box-color-1">
+				<div class="dash-box-icon">
+					<i class="glyphicon glyphicon-cloud"></i>
+				</div>
+				<div class="dash-box-body">
+					<span class="dash-box-count">8,252</span>
+					<span class="dash-box-title">Lesson Plan</span>
+				</div>
+				
+				<div class="dash-box-action">
+				    <button> <a href="result.php" > View your feedback</a></button>
+					  
+				</div>				
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="dash-box dash-box-color-2">
+				<div class="dash-box-icon">
+					<i class="glyphicon glyphicon-download"></i>
+				</div>
+				<div class="dash-box-body">
+					<span class="dash-box-count">100</span>
+					<span class="dash-box-title">Learning Resources</span>
+				</div>
+				
+				<div class="dash-box-action">
+					<button> <a href="feedback.html" > submit your feedback</a></button>
+				</div>				
+			</div>
+		</div>
+	<div class="col-md-4">
+			<div class="dash-box dash-box-color-2">
+				<div class="dash-box-icon">
+					<i class="glyphicon glyphicon-download"></i>
+				</div>
+				<div class="dash-box-body">
+					<span class="dash-box-count">100</span>
+					<span class="dash-box-title">Learning Resources</span>
+				</div>
+				
+				<div class="dash-box-action">
+					<button> <a href="feedback.html" > submit your feedback</a></button>
+				</div>				
+			</div>
+		</div>
+		</br>
+		<div class="col-md-4">
+			<div class="dash-box dash-box-color-2">
+				<div class="dash-box-icon">
+					<i class="glyphicon glyphicon-download"></i>
+				</div>
+				<div class="dash-box-body">
+					<span class="dash-box-count">2018</span>
+					<span class="dash-box-title">CALENDAR</span>
+				</div>
+				
+				<div class="dash-box-action">
+					<button> <a href="calendar.html" > View your Calendar</a></button>
+				</div>				
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="dash-box dash-box-color-2">
+				<div class="dash-box-icon">
+					<i class="glyphicon glyphicon-download"></i>
+				</div>
+				<div class="dash-box-body">
+					<span class="dash-box-count">100</span>
+					<span class="dash-box-title">Learning Resources</span>
+				</div>
+				
+				<div class="dash-box-action">
+					<button> <a href="feedback.html" > submit your feedback</a></button>
+				</div>				
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="dash-box dash-box-color-2">
+				<div class="dash-box-icon">
+					<i class="glyphicon glyphicon-download"></i>
+				</div>
+				<div class="dash-box-body">
+					<span class="dash-box-count">100</span>
+					<span class="dash-box-title">Learning Resources</span>
+				</div>
+				
+				<div class="dash-box-action">
+					<button> <a href="feedback.html" > submit your feedback</a></button>
+				</div>				
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
